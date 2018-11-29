b4accb6c160cfee8f8bff3d4bd6dc53696ab774a
Yosua Rori <yhutapea44@gmail.com>
2018-11-30 01:05:08 +0700

    Update filter dan konten lapangan

--- application/views/filter.php
+++ application/views/filter.php
@@ -1,73 +1,75 @@
 
 <br><br><br><br><br><br>
 
-
+<div class="container" style="background-color : #eee">
 <div class="panel panel-default">
   <div class="panel-heading">FILTER YOUR RESULT</div>
-  <div class="panel-body">
-    
+    ==============================================================================================================================   
+    <br> 
     OFFICIAL / NOT OFFICIAL
     <div class="dropdown">
-        <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-            All
-            <span class="caret"></span>
-        </button>
-        <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-            <li><a href="#">All</a></li>
-            <li><a href="#">Official</a></li>
-            <li><a href="#">Not Official</a></li>
-        </ul>
-    </div>
+  <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    All
+  </button>
+  <div class="dropdown-menu">
+    <a class="dropdown-item" href="#">Official</a>
+    <a class="dropdown-item" href="#">Not Official</a>
+  </div>
+</div>
+<br>
 
-        SPORT CATEGORY
+    SPORT CATEGORY
     <div class="dropdown">
-        <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-            Select
-            <span class="caret"></span>
-        </button>
-        <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-            <li><a href="#">Basket</a></li>
-            <li><a href="#">Futsal</a></li>
-            <li><a href="#">Badminton</a></li>
-        </ul>
-    </div>
+  <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Select
+  </button>
+  <div class="dropdown-menu">
+    <a class="dropdown-item" href="#">Futsal</a>
+    <a class="dropdown-item" href="#">Basket</a>
+    <a class="dropdown-item" href="#">Badminton</a>
+  </div>
+</div>
 
-        TAGS SELECTION 
+<br>
+
+    TAGS SELECTION 
     <div class="dropdown">
-        <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-            All
-            <span class="caret"></span>
-        </button>
-        <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-            <li><a href="#">Indoor</a></li>
-            <li><a href="#">Outdoor</a></li>
-        </ul>
-    </div>
+  <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    All
+  </button>
+  <div class="dropdown-menu">
+    <a class="dropdown-item" href="#">Indoor</a>
+    <a class="dropdown-item" href="#">Outdoor</a>
+  </div>
+</div>
 
-    <div class="panel panel-default">
-  <div class="panel-body">
-  <div class="list-group">
-  <a href="#" class="list-group-item list-group-item-action flex-column align-items-start active">
+</div>
+<br>
+
+<br>
+
+
+  <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
     <div class="d-flex w-100 justify-content-between">
       <h5 class="mb-1">Lapangan Futsal Speed</h5>
     </div>
-    <img src="assets/img/lapangan/futsalindoorrumput.jpg">
-    <p class="mb-1"><b>overview:</b>Alamat:Way Huwi, Jati Agung, South Lampung Regency, Lampung 35131, Indonesia</p>
+    <img src="assets/img/lapangan/futsalindoorrumput.jpg" class="img-filter">
+    <p class="mb-1"><b>overview :</b> Alamat : Way Huwi, Jati Agung, South Lampung Regency, Lampung 35131, Indonesia</p>
   </a>
 
   <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
     <div class="d-flex w-100 justify-content-between">
       <h5 class="mb-1">Lapangan Futsal 45</h5>
     </div>
-    <img src="assets/img/lapangan/futsalindoorsemen">
-    <p class="mb-1"><b>overview:</b>Lapangan futsal Indoor dengan alas yang terbuat dari rubber berkualitas internasional yang di impor dari Jerman dan telah memenuhi New European Sport Flooring Standart (EN 14904).memiliki dua lapangan futsal dengan ukuran 17,5 m x 27 m dan ukuran standart internasional 22m x 38m.</p>
+    <img src="assets/img/lapangan/futsalindoorsemen.jpg" class="img-filter">
+    <p class="mb-1"><b>overview :</b> Lapangan futsal Indoor dengan alas yang terbuat dari rubber berkualitas internasional yang di impor dari Jerman dan telah memenuhi New European Sport Flooring Standart (EN 14904).memiliki dua lapangan futsal dengan ukuran 17,5 m x 27 m dan ukuran standart internasional 22m x 38m.</p>
   </a>
   <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
     <div class="d-flex w-100 justify-content-between">
       <h5 class="mb-1">Lampung Futsal</h5>
     </div>
-    <img src="assets/img/lapangan/futsaloutdorrumput">
-    <p class="mb-1"><b>overview:bandar lampung, lapangan ini beralamatkan di : Jl. Z. A. Pagar Alam, Gg. Abdul Muis No. 2A Bandar Lampung. </b></p>
+    <img src="assets/img/lapangan/futsaloutdoorrumput.jpg" class="img-filter">
+    <p class="mb-1"><b>overview :</b> Bandar Lampung, lapangan ini beralamatkan di : Jl. Z. A. Pagar Alam, Gg. Abdul Muis No. 2A Bandar Lampung.</p>
   </a>
 </div>
   </div>
