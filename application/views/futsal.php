f868d7a1442c2e3b19bef87c4f6749b59d9d04eb
Unknown <vallenrefkiyawan123@gmail.com>
2018-11-30 02:32:38 +0700

    Tambah halaman detail

---
+++ application/views/futsal.php
@@ -0,0 +1,187 @@
+<!DOCTYPE <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Lapangan Speed</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <br><br><br><br>
+
+<div class="row">
+  <div class="col-md-12">
+    <div class="thumbnail">
+      <img src="assets/img/lapangan/futsalindoorrumput.jpg" alt="Basket Lampung Walk" class="img-detail">
+      <div class="caption">
+      <div class="detail-header">
+                    <div class="detail-header-inner">
+                      <h3>Lapangan Speed</h3>
+                      <p class="location"><i class="fa fa-map-marker"></i> Way huwi ,Jati Agung ,Lampung Selatan.</p>
+                      <p> Biaya sewa Rp.100.000 <p>
+                      <div class="rating-wrapper">
+                        <div class="rating-item">
+                          <span style="cursor: default;"><div class="rating-symbol" style="display: inline-block; position: relative;"><div class="rating-symbol-background oi oi-star" style="visibility: visible;"></div><div class="rating-symbol-foreground" style="display: inline-block; position: absolute; overflow: hidden; left: 0px; right: 0px; width: 0%;"><span></span></div></div><div class="rating-symbol" style="display: inline-block; position: relative;"><div class="rating-symbol-background oi oi-star" style="visibility: visible;"></div><div class="rating-symbol-foreground" style="display: inline-block; position: absolute; overflow: hidden; left: 0px; right: 0px; width: 0px;"><span></span></div></div><div class="rating-symbol" style="display: inline-block; position: relative;"><div class="rating-symbol-background oi oi-star" style="visibility: visible;"></div><div class="rating-symbol-foreground" style="display: inline-block; position: absolute; overflow: hidden; left: 0px; right: 0px; width: 0px;"><span></span></div></div><div class="rating-symbol" style="display: inline-block; position: relative;"><div class="rating-symbol-background oi oi-star" style="visibility: visible;"></div><div class="rating-symbol-foreground" style="display: inline-block; position: absolute; overflow: hidden; left: 0px; right: 0px; width: 0px;"><span></span></div></div><div class="rating-symbol" style="display: inline-block; position: relative;"><div class="rating-symbol-background oi oi-star" style="visibility: visible;"></div><div class="rating-symbol-foreground" style="display: inline-block; position: absolute; overflow: hidden; left: 0px; right: 0px; width: 0px;"><span class="oi oi-star"></span></div></div></span><input type="hidden" class="rating" data-filled="oi oi-star" data-empty="oi oi-star" data-fractions="2" data-readonly="" value="0">
+                        
+                    </div>
+                  </div>
+
+        </div>
+</div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<div class="row">
+
+
+<div class="col-xs-12 col-sm-6 mb-20">
+                  
+                  <div class="contact-box">
+                  
+                    <h5 class="text-primary">Contact Information</h5><br>
+                    
+                    <p>Yousa ; 089675432187</p>
+                    <a href="#detail-content-sticky-nav-04" class="btn btn-primary btn-sm anchor-alt">See map &amp; get route</a> 
+                  </div>
+        
+                </div>
+</div>
+
+<div class="row">
+
+  
+<div class="col-md-6">
+                  
+                  <div class="open-time-box text-center">
+                  
+                    <h5 class="text-primary">Opening Time</h5>
+                    
+                    <ul class="open-time-list" id="operate_time">
+                      <div class="loader" style="display: none;"></div>
+                    <li class="">
+                                            <div class="row">
+                                            
+                                              <div class="col-xs-6 col-sm-6">
+                                                <span class="day">
+                                                  Monday
+                                                </span>
+                                              </div>
+                                              
+                                              <div class="col-xs-6 col-sm-6">
+                                                  <span class="time">
+                                                   08:00:00 am  - 10:00:00 pm 
+                                                </span>
+                                              </div>
+                                              
+                                            </div>
+                                          </li><li class="">
+                                            <div class="row">
+                                            
+                                              <div class="col-xs-6 col-sm-6">
+                                                <span class="day">
+                                                  Tuesday
+                                                </span>
+                                              </div>
+                                              
+                                              <div class="col-xs-6 col-sm-6">
+                                                  <span class="time">
+                                                   08:00:00 am  - 10:00:00 pm 
+                                                </span>
+                                              </div>
+                                              
+                                            </div>
+                                          </li><li class="">
+                                            <div class="row">
+                                            
+                                              <div class="col-xs-6 col-sm-6">
+                                                <span class="day">
+                                                  Wednesday
+                                                </span>
+                                              </div>
+                                              
+                                              <div class="col-xs-6 col-sm-6">
+                                                  <span class="time">
+                                                   08:00:00 am  - 10:00:00 pm 
+                                                </span>
+                                              </div>
+                                              
+                                            </div>
+                                          </li><li class="">
+                                            <div class="row">
+                                            
+                                              <div class="col-xs-6 col-sm-6">
+                                                <span class="day">
+                                                  Thursday
+                                                </span>
+                                              </div>
+                                              
+                                              <div class="col-xs-6 col-sm-6">
+                                                  <span class="time">
+                                                   08:00:00 am  - 10:00:00 pm 
+                                                </span>
+                                              </div>
+                                              
+                                            </div>
+                                          </li><li class="">
+                                            <div class="row">
+                                            
+                                              <div class="col-xs-6 col-sm-6">
+                                                <span class="day">
+                                                  Friday
+                                                </span>
+                                              </div>
+                                              
+                                              <div class="col-xs-6 col-sm-6">
+                                                  <span class="time">
+                                                   08:00:00 am  - 10:00:00 pm 
+                                                </span>
+                                              </div>
+                                              
+                                            </div>
+                                          </li><li class="">
+                                            <div class="row">
+                                            
+                                              <div class="col-xs-6 col-sm-6">
+                                                <span class="day">
+                                                  Saturday
+                                                </span>
+                                              </div>
+                                              
+                                              <div class="col-xs-6 col-sm-6">
+                                                  <span class="time">
+                                                   08:00:00 am  - 11:00:00 pm 
+                                                </span>
+                                              </div>
+                                              
+                                            </div>
+                                          </li><li class="">
+                                            <div class="row">
+                                            
+                                              <div class="col-xs-6 col-sm-6">
+                                                <span class="day">
+                                                  Sunday
+                                                </span>
+                                              </div>
+                                              
+                                              <div class="col-xs-6 col-sm-6">
+                                                  <span class="time">
+                                                   08:00:00 am  - 11:00:00 pm 
+                                                </span>
+                                              </div>
+                                              
+                                            </div>
+                                          </li></ul>
+                    
+                  </div>
+</div>
+</div>
+</div>
+                </div>
+
+</body>
+</html>
